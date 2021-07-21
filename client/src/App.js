b29f0bd54eddcb73b1c75a309a0a5ead3fdf2f3d
vcody <codyyv@protonmail.com>
2021-07-21 00:32:00 -0700

    Added register/login form

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 import './App.css';
 
 function App() {
@@ -9,7 +12,13 @@ function App() {
     <Router>
       <Fragment>
         <Navbar />
-        <Landing />
+        <Route exact path="/" component={ Landing } />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </Switch>
+        </section>
       </Fragment>
     </Router>
   );
