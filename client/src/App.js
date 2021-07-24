e6f459ffeca112eea9a045f5d3a60544c41d524b
vcody <codyyv@protonmail.com>
2021-07-23 20:07:53 -0700

    Added posts reducer and actions

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -47,6 +48,7 @@ function App() {
               <PrivateRoute exact path="/create-profile" component={ CreateProfile } />
               <PrivateRoute exact path="/edit-profile" component={ EditProfile } />
               <PrivateRoute exact path="/add-experience" component={ AddExperience } />
+              <PrivateRoute exact path='/posts' component={ Posts } />
             </Switch>
           </section>
         </Fragment>
