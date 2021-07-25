42ef86cb0003f77b292fdf24ad28d015512b1b17
vcody <codyyv@protonmail.com>
2021-07-24 23:38:49 -0700

    Added postitem for posts page

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 
 const Posts = ({ getPosts, post: { posts, loading }}) => {
     useEffect(() => {
@@ -10,10 +11,21 @@ const Posts = ({ getPosts, post: { posts, loading }}) => {
     }, [getPosts]);
 
     return (
-        <div>
-            
-        </div>
-    )
+        loading ? <Spinner /> : (
+            <Fragment>
+                <h1 className="large text-primary">Posts</h1>
+                <p className="lead">
+                    <i className="fas fa-user"></i> Welcome to the community
+                </p>
+                { /* Post Form */ }
+                <div className="posts">
+                    {posts.map(post => (
+                        <PostItem key={post._id} post={post} />
+                    ))}
+                </div>
+            </Fragment>
+        )
+    );
 };
 
 Posts.propTypes = {
